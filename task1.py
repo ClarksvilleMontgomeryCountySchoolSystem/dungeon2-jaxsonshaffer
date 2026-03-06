a33e5848bff696a66bcb3158480437d683854c58
jaxsonshaffer <jaxson.shaffer369@students.cmcss.net>
2026-03-06 13:59:38 -0600

    .

---
+++ task1.py
@@ -0,0 +1,45 @@
+bad = r""" /|
+        /\/ |/\
+        \  ^   | /\  /\
+  (\/\  / ^   /\/  )/^ )
+   \  \/^ /\       ^  /
+    )^       ^ \     (
+   (   ^   ^      ^\  )
+    \___\/____/______/
+    [________________]
+     |              |
+     |     //\\     |
+     |    <<()>>    |
+     |     \\//     |
+      \____________/
+          |    |
+          |    |
+          |    |
+          |    |
+          |    |
+          |    |
+          |    |
+"""
+good = r"""   ______
+   ,-' ;  ! `-.
+  / :  !  :  . \
+ |_ ;   __:  ;  |
+ )| .  :)(.  !  |
+ |"    (##)  _  |
+ |  :  ;`'  (_) (
+ |  :  :  .     |
+ )_ !  ,  ;  ;  |
+ || .  .  :  :  |
+ |" .  |  :  .  |
+ |mt-2_;----.___|
+"""
+
+torch_lit = True
+outcome = print("Flicker")
+if torch_lit:
+    print(good)
+    outcome = "Flicker: we can see down the hallway"
+else:
+    print(bad)
+    outcome = "Doom, we might be cooked"
+print(outcome)
